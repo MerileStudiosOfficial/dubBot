bdd2e8e2a76167e8e6fc6c7953c623d2f0424d75
MerileStudiosOfficial <sonichedgehog1@hotmail.com>
2016-05-23 22:46:30 -0500

    Update bottest.js

--- bottest.js
+++ bottest.js
@@ -7422,6 +7422,76 @@ var BOTCOMMANDS = {
                     }
                 }
             },
+            gifCommand: {
+                command: ['gif', 'giphy'],
+                rank: 'user',
+                type: 'startsWith',
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!SETTINGS.settings.gifEnabled) return void (0);
+                    if (!BOTCOMMANDS.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                        var msg = chat.message;
+                        if (msg.length !== cmd.length) {
+                            function get_id(api_key, fixedtag, func)
+                            {
+                                // Old URL: "https://api.giphy.com/v1/gifs/random?", 
+                                $.getJSON(
+                                    "https://tv.giphy.com/v1/gifs/random?", 
+                                    { 
+                                        "format": "json",
+                                        "api_key": api_key,
+                                        "rating": rating,
+                                        "tag": fixedtag
+                                    },
+                                    function(response)
+                                    {
+                                        func(response.data.id);
+                                    }
+                                    )
+                            }
+                            var api_key = "dc6zaTOxFJmzC"; // public beta key
+                            var rating = "pg-13"; // PG 13 gifs
+                            var tag = msg.substr(cmd.length + 1);
+                            var fixedtag = tag.replace(/ /g,"+");
+                            var commatag = tag.replace(/ /g,", ");
+                            get_id(api_key, tag, function(id) {
+                                if (typeof id !== 'undefined') {
+                                    API.sendChat(botChat.subChat(botChat.getChatMessage("validgiftags"), {name: chat.un, id: id, tags: commatag}));
+                                } else {
+                                    API.sendChat(botChat.subChat(botChat.getChatMessage("invalidgiftags"), {name: chat.un, tags: commatag}));
+                                }
+                            });
+                        }
+                        else {
+                            function get_random_id(api_key, func)
+                            {
+                                $.getJSON(
+                                    "https://api.giphy.com/v1/gifs/random?", 
+                                    { 
+                                        "format": "json",
+                                        "api_key": api_key,
+                                        "rating": rating
+                                    },
+                                    function(response)
+                                    {
+                                        func(response.data.id);
+                                    }
+                                    )
+                            }
+                            var api_key = "dc6zaTOxFJmzC"; // public beta key
+                            var rating = "pg-13"; // PG 13 gifs
+                            get_random_id(api_key, function(id) {
+                                if (typeof id !== 'undefined') {
+                                    API.sendChat(botChat.subChat(botChat.getChatMessage("validgifrandom"), {name: chat.un, id: id}));
+                                } else {
+                                    API.sendChat(botChat.subChat(botChat.getChatMessage("invalidgifrandom"), {name: chat.un}));
+                                }
+                            });
+                        }
+                    }
+                }
+            },
             /* basic
             activeCommand: {
                 command: 'active',
